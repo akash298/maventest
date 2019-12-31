6be4a34f9ad3edc4e7ba308d6d0be98f27ebccae
akash298 <akashpanwar298@gmail.com>
2019-12-31 12:48:57 +0530

    akash

--- src/test/java/com/automation/qa/AppTest.java
+++ src/test/java/com/automation/qa/AppTest.java
@@ -1,38 +1,17 @@
 package com.automation.qa;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+    public static void main( String[] args )
     {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    	WebDriver driver;
+    	System.setProperty("webdriver.chrome.driver","C://Users//DELL//Desktop//driver//chromedriver.exe");
+    	driver=new ChromeDriver();
+    	
+    	driver.get("https://www.google.com");
+        System.out.println( "Hello World!" );
     }
 }
